c37df632260159c13bf87407703d9e0eed23de73
Fernando Enriquez <2013200959@untels.edu.pe>
2020-08-03 12:11:06 -0500

    SUGERENCIA - Images y constructor agregados

--- src/java/Modelo/Comentarios.java
+++ src/java/Modelo/Comentarios.java
@@ -11,4 +11,63 @@ package Modelo;
  */
 public class Comentarios {
     
+    int idsugerencia;
+    String nombre;
+    String apellidos;
+    String comentario;
+    String estado;
+
+    public Comentarios() {
+    }
+
+    public Comentarios(int idsugerencia, String nombre, String apellidos, String comentario, String estado) {
+        this.idsugerencia = idsugerencia;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.comentario = comentario;
+        this.estado = estado;
+    }
+
+    public int getIdsugerencia() {
+        return idsugerencia;
+    }
+
+    public void setIdsugerencia(int idsugerencia) {
+        this.idsugerencia = idsugerencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
 }
